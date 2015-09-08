2862c4eb5e702f6fb711e40ec1f45a59ca5d61d7
osipchuk <leveldorado>
2015-09-08 03:09:57 +0300

    controller added

--- src/AppBundle/Controller/WatchlistController.php
+++ src/AppBundle/Controller/WatchlistController.php
@@ -5,9 +5,26 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Watchlist;
 
 class WatchlistController extends Controller
 {
+    /**
+    * @Route("/watchlist/create", name="watchlist" )
+    * @method POST
+    */
+    public function createNewTickerAction(Request $request){
+       $ticker = $request->request->get('ticker');
+       $record = new Watchlist();
+       $record->setTicker($ticker['ticker']);
+       
 
+       $em = $this->getDoctrine()->getManager();
+
+       $em->persist($product);
+       $em->flush();
+       return new JsonResponse(['success'=>true]);
+    }
 }
--- web/scripts/watchlistController.js
+++ web/scripts/watchlistController.js
@@ -1,4 +1,55 @@
-var app = angular.module('main', []);
-app.controller('watchlistController', ['$scope', function($scope){
-	$scope.g = "Great";
+var app = angular.module('main', ['ngResource']);
+app.config(['$resourceProvider', function($resourceProvider) {
+  // Don't strip trailing slashes from calculated URLs
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
+app.controller('watchlistController', ['$scope', '$resource', function($scope, $resource){
+	console.log('t');
+	var tickerFactory = $resource('/app_dev.php/watchlist', null, {
+		 'getAll': {method: 'GET'},
+		 'saveItem': {method:'POST'}
+	});
+	var createTicker = $resource("/app_dev.php/watchlist/create", null, {
+		'saveTicker':{method:'POST'}
+	})
+	$scope.tickers = [];
+	$scope.createTicker = function(){
+		console.log('create');
+        var ticker = {
+        	edit: true,
+            ticker: '',
+            name_ticker: '',
+            currency: 'EURO',
+            trade: '',
+            last_price_cur: 0.0,
+            purchase_contract: 0,
+            purchase_investment: 0,
+            purchase_amount: 0,
+            optimistic_price_cur: 0.0,
+            optimistic_profit: 0,
+            optimistic_profit_percent: 0.0,
+            realistic_price_cur: 0.0,
+            realistic_profit: 0,
+            realistic_profit_percent: 0.0,
+            stop_price_cur: 0.0,
+            stop_profit: 0,
+            stop_profit_percent: 0.0,
+            ma21: '',
+            ma200: '',
+            trend: '',
+            macd: '',
+            rci: '',
+            recomendation: '',
+            status: ''
+
+        };
+        $scope.tickers.unshift(ticker);
+	};
+
+	$scope.save = function(ticker){
+        ticker.edit = false;
+        createTicker.saveTicker({'ticker':ticker}, function(res){
+             console.log(res);
+        });
+	}
 }]);
